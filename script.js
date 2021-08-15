ede0088bec82c0f19fc412a557017c68dd78967c
Caio Henrique <caiohenrique@jeraapps.com>
2021-08-15 12:54:36 -0400

    [NEW] first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttonLoading = document.querySelector('.btn-loading');
+const content = buttonLoading.textContent;
+let spinnerContainer = document.createElement('div');
+spinnerContainer.classList.add('spinner-container');
+let spinner = document.createElement('div');
+spinner.classList.add('spinner');
+spinnerContainer.appendChild(spinner);
+
+buttonLoading.addEventListener('click', (e) => {
+    e.preventDefault();
+    startLoading();
+    setTimeout(stopLoading, 2000);
+});
+
+function startLoading() {
+    document.body.insertAdjacentElement('afterBegin', spinnerContainer);
+}
+
+function stopLoading() {
+    document.body.removeChild(spinnerContainer);
+}
